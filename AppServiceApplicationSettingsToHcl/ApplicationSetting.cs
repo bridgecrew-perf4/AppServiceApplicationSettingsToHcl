a9978f81b3c1fdc3154eba6bf8726d99aa9e09be
shimabiss <shimabiss@gmail.com>
2020-12-31 00:39:45 +0900

    jsonからHCLのtxtファイルを出力する機能を追加

---
+++ AppServiceApplicationSettingsToHcl/ApplicationSetting.cs
@@ -0,0 +1,23 @@
+﻿namespace AppServiceApplicationSettingsToHcl
+{
+    /// <summary>
+    /// AppServiceのアプリケーション設定
+    /// </summary>
+    public class ApplicationSetting
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 値
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// デプロイスロットの設定
+        /// </summary>
+        public bool SlotSetting { get; set; }
+    }
+}
--- AppServiceApplicationSettingsToHcl/Program.cs
+++ AppServiceApplicationSettingsToHcl/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace AppServiceApplicationSettingsToHcl
 {
@@ -6,7 +9,37 @@ namespace AppServiceApplicationSettingsToHcl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("入力ファイルパス(.json): ");
+                string inputFilePath = Console.ReadLine();
+
+                var date = DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss");
+                string outputFilePath = $"OutputFile_{date}.txt";
+                string jsonString = File.ReadAllText(inputFilePath);
+                var data = JsonSerializer.Deserialize<List<ApplicationSetting>>(jsonString, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                if (!File.Exists(outputFilePath))
+                {
+                    using (StreamWriter sw = File.CreateText(outputFilePath))
+                    {
+                        sw.WriteLine("app_settings = {");
+                        foreach (var item in data)
+                        {
+                            sw.WriteLine($"\"{item.Name}\" = \"{item.Value}\"");
+                        }
+                        sw.WriteLine("}");
+                    }
+                }
+                    
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
